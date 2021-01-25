89a2af026b2b3351bc00a8ffe50fd21bf69d7c41
rafaelclaumann <rafael_claumann@hotmail.com>
2021-01-25 16:45:20 -0300

    atualizaçao veiculo e criação Pagamento e TipoPagamento

--- src/main/java/br/com/proposta/api/entities/Veiculo.java
+++ src/main/java/br/com/proposta/api/entities/Veiculo.java
@@ -1,15 +1,27 @@
 package br.com.proposta.api.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "TB_VEICULO")
 public class Veiculo implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private String modelo;
     private Integer ano;
     private BigDecimal valor;
 
-    public Veiculo() {
-        
-    }
 }
+
