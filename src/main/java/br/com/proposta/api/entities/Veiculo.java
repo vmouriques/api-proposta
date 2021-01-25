c62f21850387bbebf5a78fe9714b2d03afeb8264
rafaelclaumann <rafael_claumann@hotmail.com>
2021-01-25 15:55:09 -0300

    correção entidade veiculo

--- src/main/java/br/com/proposta/api/entities/Veiculo.java
+++ src/main/java/br/com/proposta/api/entities/Veiculo.java
@@ -1,4 +1,15 @@
 package br.com.proposta.api.entities;
 
-public class Veiculo {
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Veiculo implements Serializable {
+
+    private String modelo;
+    private Integer ano;
+    private BigDecimal valor;
+
+    public Veiculo() {
+        
+    }
 }
