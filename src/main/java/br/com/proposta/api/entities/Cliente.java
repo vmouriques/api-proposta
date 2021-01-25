4bea93994803e8460a03f63b0921641b55034437
vmouriques <vmouriques@gmail.com>
2021-01-25 15:31:04 -0300

    Criação da entidade Cliente

---
+++ src/main/java/br/com/proposta/api/entities/Cliente.java
@@ -0,0 +1,82 @@
+package br.com.proposta.api.entities;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private String telefone;
+	
+	public Cliente() {
+	}
+	
+	public Cliente(Long id, String nome, String cpf, String email, String telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
