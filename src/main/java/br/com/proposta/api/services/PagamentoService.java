378d3db0267cd032b390ba3892dd0b7731a7d6c4
vmouriques <vmouriques@gmail.com>
2021-01-26 15:48:17 -0300

    Criação de PropostaEntradaDTO, PropostaSaidaDTO e PagamentoService

--- src/main/java/br/com/proposta/api/services/PagamentoService.java
+++ src/main/java/br/com/proposta/api/services/PagamentoService.java
@@ -1,5 +1,11 @@
 package br.com.proposta.api.services;
 
+import br.com.proposta.api.entities.Pagamento;
+
 public class PagamentoService {
+	
+	public Pagamento pagamento() {
+		return null;
+	}
 
 }
