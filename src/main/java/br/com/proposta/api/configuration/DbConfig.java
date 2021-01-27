dc740b1224a47c8de4b89813df76e5ed5a0eadbf
vmouriques <vmouriques@gmail.com>
2021-01-27 15:38:34 -0300

    Criação DbConfig

--- src/main/java/br/com/proposta/api/configuration/DbConfig.java
+++ src/main/java/br/com/proposta/api/configuration/DbConfig.java
@@ -1,27 +1,40 @@
 package br.com.proposta.api.configuration;
 
-import br.com.proposta.api.repositories.VeiculoRepository;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.proposta.api.entities.Solicitacao;
+import br.com.proposta.api.entities.Veiculo;
+import br.com.proposta.api.repositories.SolicitacaoRepository;
+import br.com.proposta.api.repositories.VeiculoRepository;
+
 @Configuration
 public class DbConfig implements CommandLineRunner {
 
-    @Autowired
-    private VeiculoRepository veiculoRepository;
+	@Autowired
+	private VeiculoRepository veiculoRepository;
 
-    @Override
-    public void run(String... args) throws Exception {
-        // TODO Auto-generated method stub
+	@Autowired
+	private SolicitacaoRepository solicitacaoRepository;
 
-        //Solicitacao cli1 = new Solicitacao(null, "Vinicius Matos", "581.477.630-77", "vinicius@gmail.com", "(65) 65404-8485");
-        //Veiculo vei1 = new Veiculo(null, "Rolls-Royce Ghost ll 6.6 V12 Aut.", 2019, new BigDecimal("50000"));
-        //Solicitacao cli2 = new Solicitacao(null, "Rafael Claumann", "032.092.280-43", "rafael@gmail.com", "(68) 76807-0164");
-        //Veiculo vei2 = new Veiculo(null, "VW Fusca", 1961, new BigDecimal("45000"));
-        //clienteRepository.saveAll(Arrays.asList(cli1, cli2));
-        //veiculoRepository.saveAll(Arrays.asList(vei1, vei2));
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
 
-    }
+		Veiculo veiculo1 = new Veiculo(null, "Rolls-Royce Ghost ll 6.6 V12 Aut.", 2019, new BigDecimal("50000"));
+		Veiculo veiculo2 = new Veiculo(null, "VW Fusca", 1961, new BigDecimal("45000"));
+
+		veiculoRepository.saveAll(Arrays.asList(veiculo1, veiculo2));
+
+		Solicitacao solicitacao1 = new Solicitacao(null, "Vinicius Matos", "581.477.630-77", "vinicius@gmail.com", veiculo1, null);
+		Solicitacao solicitacao2 = new Solicitacao(null, "Rafael Claumann", "032.092.280-43", "rafael@gmail.com", veiculo2, null);
+
+		solicitacaoRepository.saveAll(Arrays.asList(solicitacao1, solicitacao2));
+
+	}
 
 }
